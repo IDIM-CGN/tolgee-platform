558581e126da6dff5704aac41df877a9d3a6799b
IDIM-CGN <157128615+IDIM-CGN@users.noreply.github.com>
2024-01-24 11:22:03 +0100

    Update TolgeeMore.tsx

--- webapp/src/component/security/TolgeeMore.tsx
+++ webapp/src/component/security/TolgeeMore.tsx
@@ -5,27 +5,7 @@ export function TolgeeMore() {
   const { t } = useTranslate();
   return (
     <Box mb={2}>
-      <Typography color="textSecondary" variant="h5">
-        {t('login_more_title')}
-      </Typography>
-      <Box mb={3} />
-      <Typography color="textSecondary" variant="body2" fontSize={14}>
-        <T
-          keyName="login_tolgee_website_link"
-          params={{
-            link: <Link href="https://tolgee.io" target="_blank" />,
-          }}
-        />
-      </Typography>
-
-      <Typography color="textSecondary" variant="body2" fontSize={14}>
-        <T
-          keyName="login_tolgee_documentation_link"
-          params={{
-            link: <Link href="https://tolgee.io/platform" target="_blank" />,
-          }}
-        />
-      </Typography>
+    
     </Box>
   );
 }
